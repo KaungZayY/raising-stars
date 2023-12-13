3b15c497a7d105ee64a2aaf9b56e82b7bb2b8829
KaungZayY <kaungzayyan19@gmail.com>
2023-12-13 22:17:01 +0630

    updated home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,31 +17,14 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div id="posts-container" class="text-white scrolling-pagination">
                     @foreach($posts as $post)
-                        <div class="post bg-gray-800 rounded-lg p-4 mb-4 shadow-md">
-                            <div class="flex items-center justify-between mb-2">
-                                <div class="flex items-start">
-                                    {{-- <img src="{{ $post->user->profile_image }}" alt="Profile Image" class="w-8 h-8 rounded-full mr-2"> --}}
-                                    <h2 class="text-xl font-semibold">{{ $post->user->name }}</h2>
-                                </div>
-                                <div class="flex">
-                                    <p class="text-sm text-gray-500">{{ $post->created_at->diffForHumans() }}</p>
-                                </div>
-                            </div>
-                            <div class="flex items-center justify-between mb-2">
+                        <div class="post flex flex-row justify-between border-b border-dotted border-gray-300 p-4 rounded-md">
+                            <div class="flex flex-col">
+                                <br>
+                                <h2 class="text-xl font-semibold">{{ $post->user->name }}</h2>
                                 <h3 class="text-lg font-bold mb-2">{{ $post->title }}</h3>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="4" viewBox="0 0 128 512" class="toggle-icon"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
-                            </div>
-                            <div class="flex items-center justify-between mb-2">
-                                <div class="flex items-start">
-                                    <p class="text-gray-300 mb-4">{{ $post->content }}</p>
-                                </div>
-                                <div class="option-container hidden flex-row-reverse">
-                                    <button class="bg-blue-500 text-white px-2 py-1 mb-2">Edit</button>
-                                    <button class="bg-red-500 text-white px-2 py-1 mb-2">Delete</button>
-                                </div>
-                            </div>
-                            <!-- Post Category Tags -->
-                            @if ($post->categories->isNotEmpty())
+                                <p class="text-gray-300 mb-4">{{ $post->content }}</p>
+                                <!-- Post Category Tags -->
+                                @if ($post->categories->isNotEmpty())
                                 <p class="text-gray-500">Categories: 
                                     @foreach ($post->categories as $category)
                                         {{$category->category}}
@@ -49,8 +32,25 @@
                                         {{-- above line adding coma after each loop, unless for the last item --}}
                                     @endforeach
                                 </p>
-                            @endif
+                                @endif
+                                <br>
+                            </div>
+                            <div class="flex flex-col">
+                                <p class="text-sm text-gray-500" style="margin-left: auto">{{ $post->created_at->diffForHumans() }}</p>
+                                <br>
+                                <button onclick="toggleDropdownMenu(this)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="4" viewBox="0 0 128 512" style="margin-left: auto">
+                                        <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
+                                    </svg>
+                                </button>
+                                <div class="hidden flex-row-reverse">
+                                    <br>
+                                    <button class="bg-blue-500 text-white px-2 py-1 mb-2">Edit</button>
+                                    <button class="bg-red-500 text-white px-2 py-1 mb-2">Delete</button>
+                                </div>
+                            </div>
                         </div>
+                        
                     @endforeach
                     <div style="display:none">
                         {{ $posts->links() }}
@@ -79,15 +79,23 @@
                 console.log('Error loading next page.');
             }
         });
+    });
 
-        //menu toggle
-        $('.option-container').hide();
-        $('.toggle-icon').click(function(){
-            $('.option-container').toggle();
+    //menu toggle
+    function toggleDropdownMenu(button) 
+    {
+        const dropdownMenu = button.nextElementSibling;
+        dropdownMenu.classList.toggle('hidden');
+    }
+
+    document.addEventListener('click', function (event) 
+    {
+        const dropdownMenus = document.querySelectorAll('.hidden');
+        dropdownMenus.forEach(function (menu) {
+            if (!menu.contains(event.target)) 
+            {
+                menu.classList.add('hidden');
+            }
         });
-        $('.toggle-icon').hover(
-            function() { $(this).css('cursor', 'pointer'); },
-            function() { $(this).css('cursor', 'auto'); }
-        );
     });
 </script>
