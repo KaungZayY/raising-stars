ec0c9b6672fe3ef2bcf59f90d4fb6572ecfc84af
KaungZayY <kaungzayyan19@gmail.com>
2024-01-08 11:04:31 +0630

    Handle No Course Available

--- resources/views/courses-view/courses-list.blade.php
+++ resources/views/courses-view/courses-list.blade.php
@@ -7,15 +7,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex flex-wrap mx-auto w-3/4">
-                @foreach ($courses as $course)
-                    <div class="w-1/2 text-center">
-                        <div class="flex flex-col justify-center m-3 bg-green-400 dark:bg-green-200 rounded-lg border border-gray-200">
-                            <h2 class="font-bold mt-2 mb-2">{{$course->course}}</h2>
-                            <p class="mb-2">Age : {{$course->from_age}} to {{$course->to_age}}</p>
-                            <p class="mb-2">Fees : <u>{{$course->fees}}</u> mmk</p>
+                @if ($courses->count())
+                    @foreach ($courses as $course)
+                        <div class="w-1/2 text-center">
+                            <div class="flex flex-col justify-center m-3 bg-green-400 dark:bg-green-200 rounded-lg border border-gray-200">
+                                <h2 class="font-bold mt-2 mb-2">{{$course->course}}</h2>
+                                <p class="mb-2">Age : {{$course->from_age}} to {{$course->to_age}}</p>
+                                <p class="mb-2">Fees : <u>{{$course->fees}}</u> mmk</p>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                @else
+                    <div class="flex mx-auto justify-center">
+                        <p class="text-lg text-center text-black dark:text-white">No Available Courses</p>
+                    </div>                
+                @endif
             </div>
         </div>
     </div>
