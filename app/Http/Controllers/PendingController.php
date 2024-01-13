84ee2b9e469720bdf006f6b74ab96ff2165c997e
KaungZayY <kaungzayyan19@gmail.com>
2024-01-13 16:16:25 +0630

    Pending Search Detail Fixed

--- app/Http/Controllers/PendingController.php
+++ app/Http/Controllers/PendingController.php
@@ -30,7 +30,7 @@ class PendingController extends Controller
                         ->join('users', 'schedule_student.user_id', '=', 'users.id')
                         ->join('courses', 'schedules.course_id', '=', 'courses.id')
                         ->where('users.name','like', '%' . $request->search . '%')
-                        ->select('courses.course','schedules.*','users.name','users.email','schedule_student.status')
+                        ->select('courses.course','schedules.*','users.name','users.email','schedule_student.status','schedule_student.id')
                         ->get();
 
             if($pendings)
@@ -49,7 +49,7 @@ class PendingController extends Controller
                         '<td class="py-2 px-4 border-b text-center">'.$pending->email.'</td>'.
                         '<td class="py-2 px-4 border-b text-center">'.
                             '<div class="inline-block">'.
-                                '<form action="#" method="GET">'.
+                                '<form action="'.route('pending.detail',$pending->id).'" method="GET">'.
                                     '<button>
                                         <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512">
                                             <path fill="#22c55e" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM281 385c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l71-71L136 280c-13.3 0-24-10.7-24-24s10.7-24 24-24l182.1 0-71-71c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L393 239c9.4 9.4 9.4 24.6 0 33.9L281 385z"/>
