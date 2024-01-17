8559a795897edfd312a8d20bc4af3bd02851c23b
KaungZayY <kaungzayyan19@gmail.com>
2024-01-17 12:31:53 +0630

    Group Setup

---
+++ app/Models/Group.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Group extends Model
+{
+    use HasFactory,SoftDeletes;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected array $sortables = [
+        'id',
+        'name',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class,'user_group');
+    }
+}
---
+++ database/seeders/GroupSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('groups')->insert([
+            'name' => 'Grade 9 Jan-2024',
+        ]);
+    }
+}
