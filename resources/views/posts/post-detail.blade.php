544f1bbd8c7ed71d5c06dfbc697a6f1e4dda5781
KaungZayY <kaungzayyan19@gmail.com>
2024-02-09 21:41:24 +0630

    post & group post functions moved according to requirements

--- resources/views/posts/post-detail.blade.php
+++ resources/views/posts/post-detail.blade.php
@@ -4,15 +4,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
 
-    <!-- Post.JS -->
-    <script>
-        // Define JavaScript variables with route URLs
-        const likeRoute = "{{ route('post.like') }}";
-        const unlikeRoute = "{{ route('post.unlike') }}";
-        const commentRoute = "{{ route('post.comment') }}";
-        var csrf = "{{ csrf_token() }}";
-    </script>
-    <script src="{{asset('javascript/post.js')}}"></script>
 @endpush
 
 <x-app-layout>
@@ -36,6 +27,105 @@
 </x-app-layout>
 
 <script>
+    
+    //menu toggle
+    function toggleDropdownMenu(button) 
+    {
+        const dropdownMenu = button.nextElementSibling;
+        dropdownMenu.classList.toggle('hidden');
+    }
+    function toggleCommentBox(button)
+    {
+        const commentBox = button.parentNode.parentNode.nextElementSibling;
+        commentBox.classList.toggle('hidden');
+    }
+    document.addEventListener('click', function (event) 
+    {
+        const dropdownMenus = document.querySelectorAll('.hidden');
+        dropdownMenus.forEach(function (menu) {
+            if (!menu.contains(event.target)) 
+            {
+                menu.classList.add('hidden');
+            }
+        });
+    });
+    //like post
+    function postLiked(post_id)
+    {
+        const flashMessageContainer = $('#flash-message-container');
+        $.ajax({
+            url: "{{ route('post.like') }}",
+            method: 'POST',
+            data: {
+                '_token': '{{ csrf_token() }}',
+                'post_id': post_id,
+            },
+            beforeSend: function() {
+                // Show the flash message container before the request is sent
+                flashMessageContainer.empty();
+                flashMessageContainer.show();
+            },
+            success: function() {
+                // Handle the success response, e.g., update the UI
+                $('#flash-message-container').html('<div style="background-color: #48bb78; color: #fff; padding: 0.5rem 1rem;">Post Liked</div>');
+                // Toggle between like and unlike buttons
+                $('#like-button-' + post_id).hide();
+                $('#unlike-button-' + post_id).show();
+                setTimeout(function() 
+                {
+                    flashMessageContainer.hide();
+                }, 2000);
+            },
+            error: function() {
+                // Handle the error response
+                $('#flash-message-container').html('<div style="background-color: #e40f0f; color: #fff; padding: 0.5rem 1rem;">Cannot Do this Action</div>');
+                // console.log("custom error");
+                setTimeout(function() 
+                {
+                    flashMessageContainer.hide();
+                }, 2000);
+            }
+        });
+    }
+    //Unlike Post JS Mechanic
+    function postUnLiked(post_id)
+    {
+        const flashMessageContainer = $('#flash-message-container');
+        $.ajax({
+            url: "{{ route('post.unlike') }}",
+            method: 'DELETE',
+            data: {
+                '_token': '{{ csrf_token() }}',
+                'post_id': post_id,
+            },
+            beforeSend: function() {
+                // Show the flash message container before the request is sent
+                flashMessageContainer.empty();
+                flashMessageContainer.show();
+            },
+            success: function() {
+                // Handle the success response, e.g., update the UI
+                $('#flash-message-container').html('<div style="background-color: #48bb78; color: #fff; padding: 0.5rem 1rem;">Like Removed</div>');
+                // Toggle between like and unlike buttons
+                $('#like-button-' + post_id).show();
+                $('#unlike-button-' + post_id).hide();
+                setTimeout(function() 
+                {
+                    flashMessageContainer.hide();
+                }, 2000);
+            },
+            error: function(error) {
+                // Handle the error response
+                $('#flash-message-container').html('<div style="background-color: #e40f0f; color: #fff; padding: 0.5rem 1rem;">Cannot Do this Action, Unlike</div>');
+                // console.error(error);
+                setTimeout(function() 
+                {
+                    flashMessageContainer.hide();
+                }, 2000);
+            }
+        });
+    }
+
     function postCommented(button, post_id)
     {        
         const comment = button.previousElementSibling.value;
