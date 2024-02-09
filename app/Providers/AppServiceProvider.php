89f2f7f00d1609a936f943d060909881e708f65e
KaungZayY <kaungzayyan19@gmail.com>
2024-02-09 22:06:18 +0630

    configure https for web

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading(! app()->isProduction());
+
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
